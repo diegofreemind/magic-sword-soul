ee4e9b69f595ff524cacf7a28c326edbe1d79da9
Vitor Diego <vitordiegossousa@gmail.com>
2022-04-24 16:08:13 -0300

    Feature: Base Thief entity
    - extend from Character super
    - define Thief base class
    - define Thief default properties and values

---
+++ src/Thief.ts
@@ -0,0 +1,19 @@
+import { Character } from './Character';
+
+// TODO: import class-validator
+
+export const THIEF_LIFE_DEFAULT = 15;
+export const THIEF_SKILL_DEFAULT = 10;
+export const THIEF_STRENGTH_DEFAULT = 4;
+export const THIEF_INTELLIGENCE_DEFAULT = 4;
+
+export default class Thief extends Character {
+  constructor(readonly name: string) {
+    super(
+      THIEF_LIFE_DEFAULT,
+      THIEF_SKILL_DEFAULT,
+      THIEF_STRENGTH_DEFAULT,
+      THIEF_INTELLIGENCE_DEFAULT
+    );
+  }
+}
--- test/Character.spec.ts
+++ test/Character.spec.ts
@@ -1,6 +1,7 @@
 import Warrior from '../src/Warrior';
+import Thief from '../src/Thief';
 
-test('Deve criar um novo personagem do tipo Warrior', () => {
+test('Deve instanciar um objeto da classe Warrior', () => {
   const warrior = new Warrior('Gusmng Thyeks');
   expect(warrior).toBeInstanceOf(Warrior);
 
@@ -8,4 +9,14 @@ test('Deve criar um novo personagem do tipo Warrior', () => {
   expect(warrior.getSkill).toBe(5);
   expect(warrior.getStrength).toBe(10);
   expect(warrior.getIntelligence).toBe(5);
+});
+
+test('Deve instanciar um objeto da classe Thief', () => {
+  const thief = new Thief('Fkmaas Ghetryn');
+  expect(thief).toBeInstanceOf(Thief);
+
+  expect(thief.getLife).toBe(15);
+  expect(thief.getSkill).toBe(10);
+  expect(thief.getStrength).toBe(4);
+  expect(thief.getIntelligence).toBe(4);
 });
